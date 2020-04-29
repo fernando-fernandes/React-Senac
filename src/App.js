e27157b1ea4cc9a9819afb40eb407e057279b06c
fernando-fernandes <fernando.85@outlook.com>
2020-04-28 22:40:10 -0300

    Aula 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+  const [aluno, setAluno] = useState({});
+
+  useEffect(() => {
+
+    populaAluno();
+
+    function populaAluno(){
+      setAluno({
+        nome: 'Fernando',
+        usuario: 'fer',
+        email: 'fernando.85@outlook.com'
+      });
+    }
+  }, []);
+
+  function handleCadastrar(e){
+    e.preventDefault();
+    console.log(aluno);
+  }
+
+  return(
+    <>
+      <form className="formulario" onSubmit={handleCadastrar}>
+
+        <h3>Cadastro de aluno</h3>
+
+        <div className="row">
+          <span>Nome</span>
+          <input type="text"
+            // rest operators
+
+
+          onChange={(e) => setAluno({...aluno, nome: e.target.value})} />
+        </div>
+
+        <div className="row">
+          <span>Usuário</span>
+          <input type="text" onChange={(e) => setAluno({...aluno, usuario: e.target.value})} />
+        </div>
+
+        <div className="row">
+          <span>E-mail</span>
+          <input type="text" onChange={(e) => setAluno({...aluno, email: e.target.value})} />
+        </div>
+
+        <div className="row">
+          <button onClick={handleCadastrar}>Cadastrar</button>
+        </div>
+      </form>
+    </>
+  )
+  
 }
-
 export default App;
